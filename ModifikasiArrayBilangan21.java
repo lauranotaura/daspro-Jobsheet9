86fbbb6bde3bf882b2f26b2cf98ae291544dd49f
lauranotaura <nifa.aura11@smk.belajar.id>
2025-11-05 20:38:53 +0700

    modifikasi percobaan 1

---
+++ ModifikasiArrayBilangan21.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ModifikasiArrayBilangan21 {
+   public static void main(String[] args) {
+     Scanner sc= new Scanner(System.in);
+
+//Deklarasi
+            int [] bil= {5,1286,7,200000};
+//Input
+            bil[0]= 5;
+            bil[1]= 1286;
+            bil[2]=7;
+            bil[3]=200000;
+//Output
+            System.out.println(bil[0]);
+            System.out.println(bil[1]);
+            System.out.println(bil[2]);
+            System.out.println(bil[3]);
+
+ sc.close();
+   } 
+}
