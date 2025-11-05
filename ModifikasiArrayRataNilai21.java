c241ed38c75c8fe49dbd288a71c3c9e9bb6098f3
lauranotaura <nifa.aura11@smk.belajar.id>
2025-11-05 23:46:40 +0700

    Modifikasi percobaan 3 part 2

--- ModifikasiArrayRataNilai21.java
+++ ModifikasiArrayRataNilai21.java
@@ -1,37 +1,49 @@
 import java.util.Scanner;
+
 public class ModifikasiArrayRataNilai21 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-
-//Modifikasi
-
-        //DEKLARASI
-        int[] nilaiMhs = new int[5];
-        int counterLulus = 0; 
-
-        // INPUT
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
-            nilaiMhs[i] = sc.nextInt();
-        } 
+        Scanner sc = new Scanner(System.in);
         
-       // OUTPUT
-        System.out.println("\n--- Status Kelulusan ---");
+        //INPUT
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        //DEKLARASI
+        int jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
+        double totalLulus = 0;
+        int counterLulus = 0;
+        double totalTidakLulus = 0;
+        int counterTidakLulus = 0;
+        
+        //INPUT
         for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+            
             
             if (nilaiMhs[i] > 70) {
-                System.out.println("Mahasiswa ke-" + i + " LULUS!");
-                // MODIFIKASI: Tambahkan 1 ke counterLulus jika LULUS
-                counterLulus++; 
+                totalLulus += nilaiMhs[i];
+                counterLulus++;
             } else {
-                System.out.println("Mahasiswa ke-" + i + " TIDAK LULUS!");
+                totalTidakLulus += nilaiMhs[i];
+                counterTidakLulus++;
             }
         }
-        
-        // MODIFIKASI UTAMA: Menampilkan total mahasiswa yang lulus
-        System.out.println("\nJumlah total mahasiswa yang LULUS adalah: " + counterLulus);
+        //HITUNG
+        double rataRataLulus = 0.0;
+        if (counterLulus > 0) {
+            rataRataLulus = totalLulus / counterLulus; 
+        }
+
+        double rataRataTidakLulus = 0.0;
+       
+        if (counterTidakLulus > 0) {
+            rataRataTidakLulus = totalTidakLulus / counterTidakLulus;
+        }
+
+        // OUTPUT
+        System.out.println("Rata-rata nilai lulus = " + rataRataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataRataTidakLulus);
 
         sc.close();
     }
 }
-    
