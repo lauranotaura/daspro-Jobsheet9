0422714003a77d87b6bb988dbde35d9bebb018c5
lauranotaura <nifa.aura11@smk.belajar.id>
2025-11-05 20:22:49 +0700

    Percobaan 1

---
+++ ArrayBilangan21.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+    public class ArrayBilangan21 {
+    
+        public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+
+//Deklarasi
+            int [] bil= new int[4];
+//Input
+            bil[0]= 5;
+            bil[1]= 13;
+            bil[2]=-7;
+            bil[3]=17;
+//Output
+            System.out.println(bil[0]);
+            System.out.println(bil[1]);
+            System.out.println(bil[2]);
+            System.out.println(bil[3]);
+
+ sc.close();
+
+        }
+    }
