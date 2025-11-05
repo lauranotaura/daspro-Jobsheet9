c5f1c41121f70d680e51acd77a74847f9514369b
lauranotaura <nifa.aura11@smk.belajar.id>
2025-11-05 20:30:55 +0700

    Modifikasi Percobaan 1

--- ArrayBilangan21.java
+++ ArrayBilangan21.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
             Scanner sc= new Scanner(System.in);
 
 //Deklarasi
-            int [] bil= new int[4];
+            int [] bil= {5,1286,7,20000};
 //Input
             bil[0]= 5;
-            bil[1]= 13;
-            bil[2]=-7;
-            bil[3]=17;
+            bil[1]= 1286;
+            bil[2]=7;
+            bil[3]=20000;
 //Output
             System.out.println(bil[0]);
             System.out.println(bil[1]);
